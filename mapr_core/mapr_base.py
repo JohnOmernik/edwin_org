54945d9e92c143d818babe4ee59ceae01147e718
johnomernik <john@omernik.com>
2017-08-25 09:41:11 -0500

    Updates to MapR Connect

--- mapr_core/mapr_base.py
+++ mapr_core/mapr_base.py
@@ -203,7 +203,7 @@ class Mapr(Magics):
                 print("I am sorry, I don't know what you want to do, try just %mapr for help options")
         else:
             if self.mapr_connected == True:
-                code, text = sendMaprRequest(cell)
+                code, text = self.sendMaprRequest(cell)
                 print("Code: %s\n%s" % (code, text))
             else:
                 print("Mapr is not connected: Please see help at %mapr  - To Connect: %mapr connect")
