1b63e0bdbce861ffb0c7f785dba3fb36e73cf060
johnomernik <john@omernik.com>
2017-08-28 07:24:49 -0500

    Updated Connect Function

--- mapr_core/mapr_base.py
+++ mapr_core/mapr_base.py
@@ -32,67 +32,11 @@ class Mapr(Magics):
         super(Mapr, self).__init__(shell)
         self.myip = get_ipython()
 
-
     def retConnStatus(self):
         if self.mapr_connected == True:
-            print("Mapr is currrently connected to %s" % self.mapr_base_url)
+            print("Mapr is currrently connected as %s to %s" % (self.mapr_user, self.mapr_base_url))
         else:
             print("Mapr is not connected")
-    def disconnectMapr(self):
-        if self.mapr_connected == True:
-            print("Disconnected Mapr Session from %s" % self.mapr_base_url)
-            self.session = None
-            self.mapr_base_url = None
-            self.mapr_pass = None
-            self.mapr_connected = False
-        else:
-            print("Mapr Not Currently Connected")
-
-
-    def getMapRCLDB(self):
-        r = open("/opt/mapr/conf/mapr-clusters.conf", "r")
-        m = r.read()
-        r.close()
-        confitems = m.strip().split(" ")
-        cluster_name = confitems[0]
-        cluster_secure = confitems[1].split("=")[1]
-        cluster_cldbs = confitems[2].split(",")
-        print("Searching for Active CLDB in %s" % cluster_cldbs)
-        bfound = False
-
-        for x in cluster_cldbs:
-            cldbhost = x.split(":")[0]
-            cldburl = "https://" + cldbhost + ":8443"
-            print("Trying %s" % cldburl)
-            verify = False
-            requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-            if 1 == 1:
-                s = requests.session()
-                r = s.get(cldburl, verify=verify)
-                if r.status_code == 200:
-                    print("Using %s as Active CLDB Host at %s" % (cldbhost, cldburl))
-                    self.mapr_base_url = cldburl
-                    bfound = True
-                    break
-            #except:
-            #    print("CLDB %s did not respond" % cldbhost)
-        if bfound != True:
-            raise Exception("Could not find active CLDB!")
-
-
-
-
-        return bfound
-
-
-
-    def sendMaprRequest(self, apireq):
-        s = requests.session()
-        verify = False
-        uri = self.mapr_base_url + "/rest"+ apireq
-        print("URL Requested: %s" % uri)
-        r = s.get(uri, verify=verify, auth=(self.mapr_user, self.mapr_pass))
-        return r.status_code, r.text
 
     def connectMapr(self, use_defaults=True):
         global tpass
@@ -137,6 +81,59 @@ class Mapr(Magics):
 
 
 
+    def disconnectMapr(self):
+        if self.mapr_connected == True:
+            print("Disconnected Mapr Session from %s" % self.mapr_base_url)
+            self.session = None
+            self.mapr_base_url = None
+            self.mapr_pass = None
+            self.mapr_connected = False
+        else:
+            print("Mapr Not Currently Connected")
+
+    def sendMaprRequest(self, apireq):
+        s = requests.session()
+        verify = False
+        uri = self.mapr_base_url + "/rest"+ apireq
+        print("URL Requested: %s" % uri)
+        r = s.get(uri, verify=verify, auth=(self.mapr_user, self.mapr_pass))
+        return r.status_code, r.text
+
+    def getMapRCLDB(self):
+        r = open("/opt/mapr/conf/mapr-clusters.conf", "r")
+        m = r.read()
+        r.close()
+        confitems = m.strip().split(" ")
+        cluster_name = confitems[0]
+        cluster_secure = confitems[1].split("=")[1]
+        cluster_cldbs = confitems[2].split(",")
+        print("Searching for Active CLDB in %s" % cluster_cldbs)
+        bfound = False
+
+        for x in cluster_cldbs:
+            cldbhost = x.split(":")[0]
+            cldburl = "https://" + cldbhost + ":8443"
+            print("Trying %s" % cldburl)
+            verify = False
+            requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+            if 1 == 1:
+                s = requests.session()
+                r = s.get(cldburl, verify=verify)
+                if r.status_code == 200:
+                    print("Using %s as Active CLDB Host at %s" % (cldbhost, cldburl))
+                    self.mapr_base_url = cldburl
+                    bfound = True
+                    break
+            #except:
+            #    print("CLDB %s did not respond" % cldbhost)
+        if bfound != True:
+            raise Exception("Could not find active CLDB!")
+
+        return bfound
+
+
+
+
     def runQuery(self, query):
         if query.find(";") >= 0:
             print("WARNING - Do not type a trailing semi colon on queries, your query will fail (like it probably did here)")
@@ -158,29 +155,6 @@ class Mapr(Magics):
             return r, query_time
 
 
-    def authDrill(self):
-        url = self.drill_base_url + "/j_security_check"
-        login = {'j_username': self.drill_user, 'j_password': self.drill_pass}
-
-        verify = "/etc/ssl/certs/ca-certificates.crt"
-
-        if self.drill_pin_to_ip == True:
-            verify = False
-            requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-        else:
-            verify = "/etc/ssl/certs/ca-certificates.crt"
-
-        r = self.session.post(url, data=login, headers=self.drill_headers, verify=verify)
-        if r.status_code == 200:
-            if r.text.find("Invalid username/password credentials") >= 0:
-                raise Exception("Invalid username/password credentials")
-            elif r.text.find('<li><a href="/logout">Log Out (') >= 0:
-                pass
-            else:
-                raise Exception("Unknown HTTP 200 Code: %s" % r.text)
-        else:
-            raise Exception("Status Code: %s - Error" % r.status_code)
-        return self.session
 
     @line_cell_magic
     def mapr(self, line, cell=None):
@@ -208,6 +182,8 @@ class Mapr(Magics):
                 self.connectMapr(True)
             else:
                 print("I am sorry, I don't know what you want to do, try just %mapr for help options")
+        elif cell is not None and line == "cli":
+            print("MapR CLI Requested with %s" % cell)
         else:
             if self.mapr_connected == True:
                 code, text = self.sendMaprRequest(cell)
